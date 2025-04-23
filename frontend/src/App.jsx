1e20b6a8c6b68f67568accad1f29569b4ca00f7c
TranKietHCMUS <anhkiet10h1nd@gmail.com>
2025-04-23 12:53:14 +0700

    update(app-frontend): Add page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SearchForm from './components/getUserInfoForm'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/registerForm'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <SearchForm />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchForm />} />
+        <Route path="/auth/login" element={<LoginForm />} />
+        <Route path="/auth/register" element={<RegisterForm />} />
+      </Routes>
+    </Router>
   )
 }
 
