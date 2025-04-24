b2cb0c7b1756bf167788b79bad032d0b85bb431a
TranKietHCMUS <anhkiet10h1nd@gmail.com>
2025-04-24 15:23:56 +0700

    update(AuthContext): add error, loading, info

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useCallback, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
@@ -13,55 +13,110 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Kiểm tra token trong localStorage khi component mount
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsAuthenticated(true);
-      // Có thể thêm logic để lấy thông tin user từ token ở đây
-      setUser({ id: 1 }); // Placeholder, thay thế bằng dữ liệu thực tế
+    try {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const payload = JSON.parse(atob(token.split(".")[1]));
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (payload.exp > currentTime) {
+          setIsAuthenticated(true);
+
+          setUser({
+            id: payload.id,
+            username: payload.username,
+            fullName: payload.fullName,
+            gender: payload.gender,
+            dateOfBirth: payload.dateOfBirth,
+        });
+        }
+        else {
+          localStorage.removeItem("token");
+          setIsAuthenticated(false);
+          setUser(null);
+        }
+      }
+    } catch (error) {
+        console.error("Error:", error);
+        setUser(null);
     }
-    setLoading(false);
+  }, [])
+
+  const [registerError, setRegisterError] = useState(null);
+  const [isRegisterLoading, setIsRegisterLoading] = useState(false);
+  const [registerInfo, setRegisterInfo] = useState({
+    username: "",
+    password: "",
+    fullName: "",
+    gender: "",
+    dateOfBirth: ""
+  });
+
+  const [loginError, setLoginError] = useState(null);
+  const [isLoginLoading, setIsLoginLoading] = useState(false);
+  const [loginInfo, setLoginInfo] = useState({
+    username: "",
+    password: ""
+  });
+
+  const updateLoginInfo = useCallback((info) => {
+    setLoginInfo(info);
   }, []);
 
-  const login = async (credentials) => {
-    try {
-      // Thực hiện call API login ở đây
-      // const response = await api.login(credentials);
-      // const { token, user } = response.data;
-      
-      // Giả lập đăng nhập thành công
+  const loginUser = useCallback( async(e) => {
+      // setIsLoginLoading(true);
+      // setLoginError(null);
+
+      // const response = await postLoginOrRegister(`${baseUrl}/auth/login/`, JSON.stringify(loginInfo));
+
+      // setIsLoginLoading(false);
+      // if (response.error) return setLoginError(response);
+
+      // localStorage.setItem("User", JSON.stringify(response['user']));
+      // localStorage.setItem('token', response['accessToken']);
+      // setUser(response['user']);
       const token = 'fake-token';
       const userData = { id: 1, name: 'User' };
       
       localStorage.setItem('token', token);
       setIsAuthenticated(true);
       setUser(userData);
-      return { success: true };
-    } catch (error) {
-      return { success: false, error: error.message };
-    }
-  };
+  }, [loginInfo])
 
-  const logout = () => {
-    localStorage.removeItem('token');
-    setIsAuthenticated(false);
-    setUser(null);
-  };
+  const updateRegisterInfo = useCallback((info) => {
+      setRegisterInfo(info);
+  }, []);
 
-  const value = {
-    isAuthenticated,
-    user,
-    loading,
-    login,
-    logout
-  };
+  const registerUser = useCallback( async(e) => {
+      e.preventDefault();
+      setIsRegisterLoading(true);
+      setRegisterError(null);
+
+      const response = await postLoginOrRegister(`${baseUrl}/auth/register/`, JSON.stringify(registerInfo));
+
+      setIsRegisterLoading(false);
+      if (response.error) return setRegisterError(response);
+
+  }, [registerInfo]);
+
+  const logoutUser = useCallback( async(e) => {
+      const response = await getRequest(`${baseUrl}/auth/logout?user_id=${user?.id}`);
+      if (response?.error) {
+          console.log(response?.error);
+          return;
+      }
+      localStorage.removeItem("User");
+      localStorage.removeItem("token");
+      setUser(null);
+  }, [user]);
 
   return (
-    <AuthContext.Provider value={value}>
-      {!loading && children}
+    <AuthContext.Provider value={{user, isAuthenticated,
+        registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading,
+        loginInfo, updateLoginInfo, loginError, loginUser, isLoginLoading,
+        logoutUser}}>
+      {children}
     </AuthContext.Provider>
   );
 }; 
