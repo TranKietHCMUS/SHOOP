0bc067ee8dd73f673ed92b5f8c85d9ec3066cd7b
TranKietHCMUS <anhkiet10h1nd@gmail.com>
2025-04-25 11:13:03 +0700

    update(app): update route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,9 @@ import { AuthProvider } from './contexts/AuthContext';
 import Phase1ResultPrototype from './pages/Phase1ResultPrototype';
 import FailRequest from './pages/FailRequest';
 import Phase1Result from './pages/Phase1Result';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/registerForm';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -14,12 +15,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/get-user-data" element={<GetUserData />} />
           <Route path="/phase1-result-prototype" element={<Phase1ResultPrototype />} />
           <Route path="/error" element={<FailRequest />} />
           <Route path="/phase1-result" element={<Phase1Result />} />
+          <Route path="/result" element={<MainPage />} />
         </Routes>
       </Router>
     </AuthProvider>
