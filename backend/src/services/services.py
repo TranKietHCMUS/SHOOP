a3e2d6909a3334ed714a999588e4b19eadf7e31d
TranKietHCMUS <anhkiet10h1nd@gmail.com>
2025-04-25 11:07:41 +0700

    update(user-service instance): add params

---
+++ backend/src/services/services.py
@@ -0,0 +1,8 @@
+from src.extensions import db
+from src.services.product_vec_service import ProductVecService
+from src.services.ai_service import AIService
+from src.services.user_service import UserService
+
+product_vec_service = ProductVecService(db)
+ai_service = AIService(product_vec_service=product_vec_service)
+user_service = UserService(ai_service=ai_service, db=db)
