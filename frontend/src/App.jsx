214a4fa6790cbecde9ca0bfaf96b0e8a0d638f6c
TranKietHCMUS <anhkiet10h1nd@gmail.com>
2025-04-26 19:55:08 +0700

    update(app): add profile page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { Home, GetUserData } from './pages';
-import { AuthProvider, useAuth } from './contexts/AuthContext'; // Import useAuth từ context
+import { AuthProvider } from './contexts/AuthContext'; // Import useAuth từ context
 import Phase1ResultPrototype from './pages/Phase1ResultPrototype';
 import FailRequest from './pages/FailRequest';
 import Phase1Result from './pages/Phase1Result';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-
-function PrivateRoute({ children }) {
-  const { isAuthenticated } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/");
-    }
-  }, [isAuthenticated, navigate]);
-
-  return children;
-}
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -28,20 +16,14 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/login"
-            element={
-              <PrivateRoute>
-                <LoginPage />
-              </PrivateRoute>
-            }
-          />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/get-user-data" element={<GetUserData />} />
           <Route path="/phase1-result-prototype" element={<Phase1ResultPrototype />} />
           <Route path="/error" element={<FailRequest />} />
           <Route path="/phase1-result" element={<Phase1Result />} />
           <Route path="/result" element={<MainPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </Router>
     </AuthProvider>
