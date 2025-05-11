4dc5b11a1e29f777fa963d1264c411a96672c88d
Trần Như Anh Kiệt <116786851+TranKietHCMUS@users.noreply.github.com>
2025-05-11 12:16:09 +0700

    update(data-pipeline): add load_to_db()

--- pipeline/airflow/dags/tasks/load_to_db.py
+++ pipeline/airflow/dags/tasks/load_to_db.py
@@ -1,2 +1,19 @@
+import requests
+import json
+from tasks.helper import minio_client, Config
+from datetime import datetime
+
+now = datetime.now()
+
+
 def load_to_db():
-    print("Loading data to database...")
+    minio_response = minio_client.get_object(Config.BRONZE_BUCKET_NAME, f"bhx/{now.year}/{now.month:02}/{now.day:02}/product.json")
+
+    data = json.load(minio_response)
+
+    url = 'http://localhost:5000/product/admin/insert'
+
+    response = requests.post(url, json=data)
+
+    print(f'Status Code: {response.status_code}')
+    print(f'Response: {response.text}')
